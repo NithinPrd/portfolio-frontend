18aa866b37a912bef749c36390f95c2dda264749
NithinPrd <43707650+NithinPrd@users.noreply.github.com>
2022-03-21 00:43:09 +0530

    Changes to page header (#1)
    * Changes to page header
    
    * Removed unnecessary import

--- src/components/header/HeaderOption.js
+++ src/components/header/HeaderOption.js
@@ -1,50 +1,10 @@
-import React from "react"
 import { Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
-class HeaderOption extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: 0,
-            color: "blue"
-        }
-    }
-
-    changeColor() {
-        console.log(this.state.value + " - Changing color")
-        var newColor = null
-        if(this.state.previousColor === 'red') {
-            newColor = "blue"
-        } else {
-            newColor = "red"
-        }
-        this.setState({
-            color: newColor,
-            previousColor: newColor,
-            value: this.state.value + 1
-        })
-    }
-
-    hoverStart() {
-        this.setState({
-            previousColor: this.state.color,
-            color: "yellow",
-        })
-    }
-
-    hoverEnd() {
-        this.setState({
-            color: this.state.previousColor
-        })
-    }
-
-    render() {
-        return(
-            <Nav.Item onClick={this.changeColor.bind(this)} onMouseEnter={this.hoverStart.bind(this)} onMouseLeave={this.hoverEnd.bind(this)}>
-            <Nav.Link style={{color: this.state.color}} color="{this.state.color}">{this.props.name} - {this.state.value}</Nav.Link>
-            </Nav.Item>
-        )
-    }
+export default function HeaderOption(props) {
+    return(
+        <Nav.Link>
+            <Link to={props.url} class="Avoid-style">{props.title}</Link>
+        </Nav.Link>
+    )
 }
-
-export default HeaderOption
